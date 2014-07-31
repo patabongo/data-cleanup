0ca2d8780a45260094b5ad16006093a7691c6d10
Stu <patabongo@gmail.com>
2014-07-31 16:52:40 +0100

    I comically didn't understand absolute paths vs. URLs. Cleaning up.

--- src/data_cleanup/view.clj
+++ src/data_cleanup/view.clj
@@ -8,12 +8,12 @@
       (vector :p (vector :a {:href (str "/analyte/" (get x :id))} (get x :name))))))
 
 (defn page-template
-  [title navigation header body]
+  [title header body]
   (html5
     [:head
      [:title title]]
     [:body
-     navigation
+     [:a {:href "/"} "Home"]
      header
      body]))
 
@@ -25,7 +25,6 @@
   [request]
   (page-template
     "Select analyte"
-    [:a {:href (str "http://" (get-hostname (:headers request)))} "Home"]
     [:h1 "Select analyte"]
     (name-id-list (get-all-analytes))))
 
@@ -33,7 +32,6 @@
   [request]
   (page-template
     "Foopage"
-    [:a {:href (str "http://" (get-hostname (:headers request)))} "Home"]
     [:h1 "Foopage"]
     [:p (str (:params request))]))
 
@@ -41,6 +39,5 @@
   [request]
   (page-template
     "Barpage"
-    [:a {:href (str "http://" (get-hostname (:headers request)))} "Home"]
     [:h1 "Barpage"]
     [:p "Blank"]))
